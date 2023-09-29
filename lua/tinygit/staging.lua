c3be8c364f3b2dcca8b01b39e0bcf801da8f42b1
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-09-29 15:11:29 +0200

    fix: async consideration for stage with hunk

--- lua/tinygit/staging.lua
+++ lua/tinygit/staging.lua
@@ -2,6 +2,8 @@ local M = {}
 local u = require("tinygit.utils")
 --------------------------------------------------------------------------------
 
+---@deprecated
+---TODO make PR to gitsigns.nvim instead?
 function M.stageHunkWithInfo()
 	-- stage
 	local ok, gitsigns = pcall(require, "gitsigns")
@@ -11,11 +13,14 @@ function M.stageHunkWithInfo()
 	end
 	gitsigns.stage_hunk()
 
-	-- display total stage info
-	local info = vim.fn.system { "git", "diff", "--staged", "--stat" }
-	if vim.v.shell_error ~= 0 then return "" end
-	local infoNoLastLine = info:gsub("\n.-$", "")
-	u.notify(infoNoLastLine, "info", "Staged Changes")
+	-- HACK defer since stage_hunk is async
+	vim.defer_fn(function()
+		-- display total stage info
+		local info = vim.fn.system { "git", "diff", "--staged", "--stat" }
+		if vim.v.shell_error ~= 0 then return "" end
+		local infoNoLastLine = info:gsub("\n.-$", "")
+		u.notify(infoNoLastLine, "info", "Staged Changes")
+	end, 100)
 end
 
 --------------------------------------------------------------------------------
