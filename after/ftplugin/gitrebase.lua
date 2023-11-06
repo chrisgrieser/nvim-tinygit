24a2a71e359bf62dc9fc0343b8f22705db594420
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2023-11-06 18:40:49 +0100

    refactor(rebase): use namespaces for highlights

--- after/ftplugin/gitrebase.lua
+++ after/ftplugin/gitrebase.lua
@@ -4,20 +4,23 @@ if vim.g.tinygit_no_rebase_ftplugin then return end
 --------------------------------------------------------------------------------
 
 -- BETTER HIGHLIGHTING
+local ns = vim.api.nvim_create_namespace("tinygit.selector")
+vim.api.nvim_win_set_hl_ns(0, ns)
+
 vim.fn.matchadd("tinygit_rebase_issueNumber", [[#\d\+]])
-vim.api.nvim_set_hl(0, "tinygit_rebase_issueNumber", { link = "Number" })
+vim.api.nvim_set_hl(ns, "tinygit_rebase_issueNumber", { link = "Number" })
 
 vim.fn.matchadd("tinygit_rebase_mdInlineCode", [[`.\{-}`]]) -- .\{-} = non-greedy quantifier
-vim.api.nvim_set_hl(0, "tinygit_rebase_mdInlineCode", { link = "@text.literal" })
+vim.api.nvim_set_hl(ns, "tinygit_rebase_mdInlineCode", { link = "@text.literal" })
 
 vim.fn.matchadd(
 	"tinygit_rebase_conventionalCommit",
 	[[\v (feat|fix|test|perf|build|ci|revert|refactor|chore|docs|break|improv)(!|(.{-}))?\ze:]]
 )
-vim.api.nvim_set_hl(0, "tinygit_rebase_conventionalCommit", { link = "Title" })
+vim.api.nvim_set_hl(ns, "tinygit_rebase_conventionalCommit", { link = "Title" })
 
 vim.fn.matchadd("tinygit_rebase_fixupSquash", [[\v (fixup|squash)!]])
-vim.api.nvim_set_hl(0, "tinygit_rebase_fixupSquash", { link = "WarningMsg" })
+vim.api.nvim_set_hl(ns, "tinygit_rebase_fixupSquash", { link = "WarningMsg" })
 
 --------------------------------------------------------------------------------
 -- KEYMAPS
