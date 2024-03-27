173f1ec74b1b3a1ce33e3d773ca7b782790445bb
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-03-27 13:03:19 +0100

    chore: minor code cleanup

--- lua/tinygit/commands/undo-last-commit.lua
+++ lua/tinygit/commands/undo-last-commit.lua
@@ -1,17 +1,15 @@
 local M = {}
-local fn = vim.fn
 local u = require("tinygit.shared.utils")
-
 --------------------------------------------------------------------------------
 
 function M.undoLastCommit()
 	if u.notInGitRepo() then return end
 
-	local response = fn.system({ "git", "reset", "--mixed", "HEAD~1" }):gsub("\n$", "")
+	local response = vim.trim(vim.fn.system({ "git", "reset", "--mixed", "HEAD~1" }))
 	if u.nonZeroExit(response) then return end
 
-	u.notify(response, "info", "Git Undo Last Commit")
-	vim.cmd.checktime()
+	u.notify(response, "info", "Undo Last Commit")
+	vim.cmd.checktime() -- updates the current buffer
 end
 
 --------------------------------------------------------------------------------
