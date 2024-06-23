2439d5dd9c69d83e18e6b56db43b78a55324bea6
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-06-23 13:34:55 +0200

    refactor: commit preview

--- lua/tinygit/commands/commit-and-amend.lua
+++ lua/tinygit/commands/commit-and-amend.lua
@@ -304,11 +304,10 @@ local function showCommitPreview()
 	local specialWhitespace = " " -- HACK to force nvim-notify to keep the blank line
 	if willStageAllChanges then
 		-- include new files in diff stats
-		local newFiles = u.syncShellCmd { "git", "ls-files", "--others", "--exclude-standard" }
-		if newFiles ~= "" then
-			for _, file in ipairs(vim.split(newFiles, "\n")) do
-				vim.system({ "git", "add", "--intent-to-add", "--", file }):wait()
-			end
+		local gitLsResponse = u.syncShellCmd { "git", "ls-files", "--others", "--exclude-standard" }
+		local newFiles = gitLsResponse ~= "" and vim.split(gitLsResponse, "\n") or {}
+		for _, file in ipairs(newFiles) do
+			vim.system({ "git", "add", "--intent-to-add", "--", file }):wait()
 		end
 
 		title = "Stage & " .. title
