51314a3884b20b27cfe71f9def39483aa076666d
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-08-08 15:18:15 +0200

    refactor: split up staging module

--- lua/tinygit/commands/staging.lua
+++ lua/tinygit/commands/staging.lua
@@ -20,7 +20,7 @@ local function notify(msg, level, extraOpts) u.notify(msg, level, "Staging", ext
 --------------------------------------------------------------------------------
 
 ---@return number
-local function getContextSize()
+function M.getContextSize()
 	-- CAVEAT context=0 is not supported without `--unidiff-zero`
 	-- DOCS https://git-scm.com/docs/git-apply#Documentation/git-apply.txt---unidiff-zero
 	-- However, it is discouraged in the git manual, and the `git apply` tends to
@@ -123,7 +123,7 @@ end
 ---@param hunk Hunk
 ---@param mode "toggle" | "reset"
 ---@return boolean success
-local function applyPatch(hunk, mode)
+function M.applyPatch(hunk, mode)
 	local args = {
 		"git",
 		"apply",
@@ -149,166 +149,6 @@ local function applyPatch(hunk, mode)
 	return success
 end
 
----@param hunks Hunk[]
-local function telescopePickHunk(hunks)
-	local pickers = require("telescope.pickers")
-	local telescopeConf = require("telescope.config").values
-	local actionState = require("telescope.actions.state")
-	local actions = require("telescope.actions")
-	local finders = require("telescope.finders")
-	local previewers = require("telescope.previewers")
-
-	local opts = require("tinygit.config").config.staging
-	local setDiffBuffer = require("tinygit.shared.diff").setDiffBuffer
-
-	---@param _hunks Hunk[]
-	local function newFinder(_hunks)
-		return finders.new_table {
-			results = _hunks,
-			entry_maker = function(hunk)
-				local entry = { value = hunk }
-
-				-- search for filenames, but also changed line contents
-				local changeLines = vim.iter(vim.split(hunk.patch, "\n"))
-					:filter(function(line) return line:match("^[+-]") end)
-					:join("\n")
-				entry.ordinal = hunk.relPath .. "\n" .. changeLines
-
-				-- format: status, filename, lnum, added, removed
-				entry.display = function(_entry)
-					---@type Hunk
-					local h = _entry.value
-					local changeWithoutHunk = h.lnum == -1
-
-					local name = vim.fs.basename(h.relPath)
-					local added = h.added > 0 and (" +" .. h.added) or ""
-					local del = h.removed > 0 and (" -" .. h.removed) or ""
-					local location = ""
-					if h.fileMode == "new" then
-						added = added .. " (new file)"
-					elseif h.fileMode == "deleted" then
-						del = del .. " (deleted file)"
-					elseif changeWithoutHunk then
-						location = h.fileMode == "binary" and " (binary)" or " (renamed)"
-					else
-						location = ":" .. h.lnum
-						if h.fileMode == "renamed" then location = location .. " (renamed)" end
-					end
-					local status = h.alreadyStaged and opts.stagedIndicator
-						or (" "):rep(vim.api.nvim_strwidth(opts.stagedIndicator))
-
-					local out = status .. name .. location .. added .. del
-					local statPos = #status + #name + #location
-					local highlights = {
-						{ { 0, 1 }, "diffChanged" }, -- status
-						{ { #status + #name, statPos }, "Comment" }, -- lnum
-						{ { statPos, statPos + #added }, "diffAdded" }, -- added
-						{ { statPos + #added + 1, statPos + #added + #del }, "diffRemoved" }, -- removed
-					}
-
-					return out, highlights
-				end
-
-				return entry
-			end,
-		}
-	end
-
-	-- DOCS https://github.com/nvim-telescope/telescope.nvim/blob/master/developers.md
-	pickers
-		.new({}, {
-			prompt_title = "Git Hunks",
-			sorter = telescopeConf.generic_sorter {},
-
-			layout_strategy = "horizontal",
-			layout_config = {
-				horizontal = {
-					preview_width = 0.65,
-					height = { 0.7, min = 20 },
-				},
-			},
-
-			finder = newFinder(hunks),
-
-			-- DOCS `:help telescope.previewers`
-			previewer = previewers.new_buffer_previewer {
-				define_preview = function(self, entry)
-					local bufnr = self.state.bufnr
-					local hunk = entry.value
-					local diffLines = vim.split(hunk.patch, "\n")
-					local ft = vim.filetype.match { filename = vim.fs.basename(hunk.relPath) }
-					setDiffBuffer(bufnr, diffLines, ft, false)
-				end,
-				dyn_title = function(_, entry)
-					local hunk = entry.value
-					if hunk.added + hunk.removed == 0 then return hunk.relPath end -- renamed w/o changes
-					local stats = ("(+%d -%d)"):format(hunk.added, hunk.removed)
-					if hunk.added == 0 then stats = ("(-%d)"):format(hunk.removed) end
-					if hunk.removed == 0 then stats = ("(+%d)"):format(hunk.added) end
-					return hunk.relPath .. " " .. stats
-				end,
-			},
-
-			attach_mappings = function(prompt_bufnr, map)
-				local function refreshPicker()
-					-- temporarily register a callback which keeps selection on refresh
-					-- SOURCE https://github.com/nvim-telescope/telescope.nvim/blob/bfcc7d5c6f12209139f175e6123a7b7de6d9c18a/lua/telescope/builtin/__git.lua#L412-L421
-					local picker = actionState.get_current_picker(prompt_bufnr)
-					local selection = picker:get_selection_row()
-					local callbacks = { unpack(picker._completion_callbacks) } -- shallow copy
-					picker:register_completion_callback(function(self)
-						self:set_selection(selection)
-						self._completion_callbacks = callbacks
-					end)
-
-					picker:refresh(newFinder(hunks), { reset_prompt = false })
-				end
-
-				map({ "n", "i" }, opts.keymaps.gotoHunk, function()
-					local hunk = actionState.get_selected_entry().value
-					actions.close(prompt_bufnr)
-					-- hunk lnum starts at beginning of context, not change
-					local hunkStart = hunk.lnum + getContextSize()
-					vim.cmd(("edit +%d %s"):format(hunkStart, hunk.absPath))
-				end, { desc = "Goto Hunk" })
-
-				map({ "n", "i" }, opts.keymaps.stagingToggle, function()
-					local entry = actionState.get_selected_entry()
-					local hunk = entry.value
-					local success = applyPatch(hunk, "toggle")
-					if success then
-						-- Change value for selected hunk in cached hunk-list
-						hunks[entry.index].alreadyStaged = not hunks[entry.index].alreadyStaged
-						if opts.moveToNextHunkOnStagingToggle then
-							actions.move_selection_next(prompt_bufnr)
-						end
-						refreshPicker()
-					end
-				end, { desc = "Staging Toggle" })
-
-				map({ "n", "i" }, opts.keymaps.resetHunk, function()
-					local entry = actionState.get_selected_entry()
-					local hunk = entry.value
-
-					-- a staged hunk cannot be reset, so we unstage it first
-					if hunk.alreadyStaged then
-						local success1 = applyPatch(hunk, "toggle")
-						if not success1 then return end
-						hunk.alreadyStaged = false
-					end
-
-					local success2 = applyPatch(hunk, "reset")
-					if not success2 then return end
-					table.remove(hunks, entry.index) -- remove from list as not a hunk anymore
-					refreshPicker()
-				end, { desc = "Reset Hunk" })
-
-				return true -- keep default mappings
-			end,
-		})
-		:find()
-end
-
 --------------------------------------------------------------------------------
 
 function M.interactiveStaging()
@@ -330,7 +170,7 @@ function M.interactiveStaging()
 	-- GET ALL HUNKS
 	u.intentToAddUntrackedFiles() -- include untracked files, enables using `--diff-filter=A`
 
-	local diffArgs = { "git", "diff", "--unified=" .. getContextSize(), "--diff-filter=ADMR" }
+	local diffArgs = { "git", "diff", "--unified=" .. M.getContextSize(), "--diff-filter=ADMR" }
 	-- no trimming, since trailing empty lines can be blank context lines in diff output
 	local changesDiff = u.syncShellCmd(diffArgs, "notrim")
 	local changedHunks = getHunksFromDiffOutput(changesDiff, false)
@@ -347,7 +187,7 @@ function M.interactiveStaging()
 		pattern = "TelescopeResults",
 		callback = function(ctx) require("tinygit.shared.backdrop").new(ctx.buf) end,
 	})
-	telescopePickHunk(allHunks)
+	require("tinygit.commands.staging.telescope").pickHunk(allHunks)
 end
 --------------------------------------------------------------------------------
 return M
