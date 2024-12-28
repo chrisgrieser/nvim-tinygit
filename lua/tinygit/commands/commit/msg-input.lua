2c815c46c7e025f133d1f1afbc3925b915a5da4e
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-12-28 17:57:43 +0100

    feat: styling of the new commit message window

--- lua/tinygit/commands/commit/msg-input.lua
+++ lua/tinygit/commands/commit/msg-input.lua
@@ -1,5 +1,5 @@
-local u = require("tinygit.shared.utils")
 local backdrop = require("tinygit.shared.backdrop")
+local u = require("tinygit.shared.utils")
 
 local M = {}
 local state = {
@@ -67,7 +67,7 @@ local function getCommitPreview(willStageAllChanges, statsWidth)
 	return changes
 end
 
----@param confirmationCallback fun(commitTitle: string, commitBody: string)
+---@param confirmationCallback fun(commitTitle: string, commitBody?: string)
 local function setupKeymaps(confirmationCallback)
 	local bufnr = state.bufnr
 	local conf = require("tinygit.config").config.commit
@@ -110,38 +110,38 @@ local function setupKeymaps(confirmationCallback)
 		end
 
 		-- confirm
-		local commitBody = vim
+		local bodytext = vim
 			.iter(vim.api.nvim_buf_get_lines(bufnr, 1, -1, false))
 			:skip(1) -- skip title
 			:join("\n") -- join for shell command
-		confirmationCallback(commitTitle, vim.trim(commitBody))
+		local commitBody = vim.trim(bodytext) ~= "" and vim.trim(bodytext) or nil
+		confirmationCallback(commitTitle, commitBody)
 
 		-- close win
 		vim.cmd.bwipeout(bufnr)
 	end)
 end
 
-local function setupFooter()
+---@param borderChar string
+local function setupTitleCharCount(borderChar)
 	local bufnr, winid = state.bufnr, state.winid
 
-	local function updateFooter()
+	local function updateTitleCharCount()
 		local titleChars = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1]:len()
 		local countHighlight = titleChars <= MAX_TITLE_LEN and "FloatBorder" or "ErrorMsg"
-		vim.api.nvim_win_set_config(winid, {
-			footer = {
-				{ " ", "FloatBorder" },
-				{ tostring(titleChars), countHighlight },
-				{ "/" .. MAX_TITLE_LEN .. " ", "FloatBorder" },
-			},
-			footer_pos = "right",
-		})
+
+		local winConf = vim.api.nvim_win_get_config(winid)
+		winConf.footer[#winConf.footer - 2] = { titleChars < 10 and borderChar or "", "FloatBorder" }
+		winConf.footer[#winConf.footer - 1] = { " " .. tostring(titleChars), countHighlight }
+
+		vim.api.nvim_win_set_config(winid, winConf)
 	end
 
-	updateFooter() -- initialize
+	updateTitleCharCount() -- initialize
 	vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
 		desc = "Tinygit: update char count in input window",
 		buffer = bufnr,
-		callback = updateFooter,
+		callback = updateTitleCharCount,
 	})
 end
 
@@ -158,18 +158,43 @@ local function setupUnmount()
 	})
 end
 
+local function setupSeparator(width)
+	local function updateSeparator()
+		local ns = vim.api.nvim_create_namespace("tinygit.commitMsgInput")
+		vim.api.nvim_buf_clear_namespace(state.bufnr, ns, 0, -1)
+		local separator = { char = "┄", hlgroup = "Comment" }
+
+		vim.api.nvim_buf_set_extmark(state.bufnr, ns, 0, 0, {
+			virt_lines = {
+				{ { separator.char:rep(width), separator.hlgroup } },
+			},
+			virt_lines_leftcol = true,
+		})
+	end
+	updateSeparator() -- initialize
+
+	-- ensure the separator is always there, even if user has deleted first line
+	vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+		desc = "Tinygit: update separator in input window",
+		buffer = state.bufnr,
+		callback = updateSeparator,
+	})
+end
+
 --------------------------------------------------------------------------------
 
 ---@param mode "commit"|"amend"
 ---@param prompt string
----@param confirmationCallback fun(commitTitle: string, commitBody: string)
+---@param confirmationCallback fun(commitTitle: string, commitBody?: string)
 function M.new(mode, prompt, confirmationCallback)
 	state.commitMode = mode
 
 	-- PARAMS
 	local config = require("tinygit.config").config
-	local width = MAX_TITLE_LEN
-	local height = 3
+	local width = MAX_TITLE_LEN + 1
+	local border = config.commit.border
+	local borderChar = border == "double" and "═" or "─"
+	local height = 4
 	prompt = vim.trim(config.appearance.mainIcon .. " " .. prompt)
 
 	-- PREFILL
@@ -177,12 +202,35 @@ function M.new(mode, prompt, confirmationCallback)
 	if mode == "amend" then
 		local lastCommitTitle = u.syncShellCmd { "git", "log", "--max-count=1", "--pretty=%s" }
 		local lastCommitBody = u.syncShellCmd { "git", "log", "--max-count=1", "--pretty=%b" }
-		msgLines = { lastCommitTitle, "", lastCommitBody }
+		msgLines = { lastCommitTitle, lastCommitBody }
 	elseif mode == "commit" then
 		local cwd = vim.uv.cwd() or ""
 		local lastCommitMsg = state.abortedCommitMsg[cwd]
 		msgLines = lastCommitMsg or {}
 	end
+	while #msgLines < 2 do -- so there is always a body
+		table.insert(msgLines, "")
+	end
+
+	-- FOOTER
+	local maps = config.commit.normalModeKeymaps
+	local hlgroup = { key = "Comment", desc = "NonText" }
+	local keymapHints = {
+		{ borderChar, "FloatBorder" }, -- extend border to align with padding
+		{ " normal mode: ", hlgroup.desc },
+		{ maps.confirm, hlgroup.key },
+		{ " confirm  ", hlgroup.desc },
+		{ maps.abort, hlgroup.key },
+		{ " abort ", hlgroup.desc },
+	}
+
+	local titleCharCount = {
+		{ borderChar:rep(5), "FloatBorder" },
+		{ borderChar, "FloatBorder" }, -- extend border if title count < 10
+		{ " 0", "FloatBorder" }, -- initial count
+		{ "/" .. MAX_TITLE_LEN .. " ", "FloatBorder" },
+	}
+	local footer = vim.list_extend(keymapHints, titleCharCount)
 
 	-- CREATE WINDOW & BUFFER
 	local bufnr = vim.api.nvim_create_buf(false, true)
@@ -195,23 +243,29 @@ function M.new(mode, prompt, confirmationCallback)
 		width = width,
 		height = height,
 		title = " " .. prompt .. " ",
-		border = config.commit.border,
+		footer = footer,
+		footer_pos = "right",
+		border = border,
 		style = "minimal",
 	})
 	vim.wo[winid].winfixbuf = true
-	vim.wo[winid].statuscolumn = " " -- = left-padding
+	vim.wo[winid].scrolloff = 0
+	vim.wo[winid].sidescrolloff = 1
+	vim.wo[winid].statuscolumn = " " -- just for left-padding
+	vim.wo[winid].colorcolumn = tostring(MAX_TITLE_LEN + 1)
+	vim.wo[winid].winhighlight = "@markup.heading.gitcommit:" -- no hl, our separator is enough
+	vim.wo[winid].list = true
+	vim.wo[winid].listchars = "precedes:…,extends:…"
+
+	vim.cmd.startinsert { bang = true }
 	state.winid, state.bufnr = winid, bufnr
 
-	-- BASE SETUP
-	vim.cmd.startinsert { bang = true }
-	setupKeymaps(confirmationCallback)
-
 	-- AUTOCMDS
-	setupFooter()
-	setupUnmount()
-
-	-- APPEARANCE
 	backdrop.new(bufnr)
+	setupKeymaps(confirmationCallback)
+	setupTitleCharCount(borderChar)
+	setupUnmount()
+	setupSeparator(width)
 end
 
 --------------------------------------------------------------------------------
