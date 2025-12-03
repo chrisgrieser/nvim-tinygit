d4366d275248fd1b94bf09c526b95e583e02d277
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-12-03 17:54:30 +0100

    feat: highlights of commit messages correctly consider scope

--- lua/tinygit/shared/highlights.lua
+++ lua/tinygit/shared/highlights.lua
@@ -10,14 +10,23 @@ end
 function M.commitType(startingFromLine)
 	local prefix = startingFromLine and "\\%>" .. startingFromLine .. "l" or ""
 
+	-- TYPE
 	-- not restricted to start of string, so prefixes like the numbering from the
-	-- snacks-ui-select do not prevent the highlight
+	-- `snacks-ui-select` do not prevent the highlight
 	local commitTypePattern = [[\v\w+(\(.{-}\))?!?]]
-
-	local type = prefix .. commitTypePattern .. [[\ze: ]] -- `\ze`: end of match
+	local commitType = prefix .. commitTypePattern .. [[\ze: ]] -- `\ze`: end of match
+	vim.fn.matchadd("@keyword.gitcommit", commitType)
 	local colonAfterType = prefix .. commitTypePattern .. [[\zs: ]] -- `\zs`: start of match
-	vim.fn.matchadd("Keyword", type)
 	vim.fn.matchadd("Comment", colonAfterType)
+
+	-- SCOPE
+	local commitScopeBrackets = prefix .. [[\v\w+\zs(\(.{-}\)\ze): ]] -- matches scope with brackets
+	vim.fn.matchadd("Comment", commitScopeBrackets, 10) -- lower prio than scope-matching for simple pattern
+	local commitScope = prefix .. [[\v\w+(\(\zs.{-}\ze\)): ]] -- matches scope without brackets
+	vim.fn.matchadd("@variable.parameter.gitcommit", commitScope, 11)
+
+	-- BANG
+	vim.fn.matchadd("@punctuation.special.gitcommit", [[\v\w+(\(.{-}\))?\zs!\ze: ]])
 end
 
 --------------------------------------------------------------------------------
